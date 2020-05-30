4824be44ed70088ab3180357770e511269c2459c
ryo.watanabe <naberyo34@gmail.com>
2020-05-30 14:22:00 +0900

    add: 14-16節

---
+++ src/array.ts
@@ -0,0 +1,12 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+let strings: string[] = ['TypeScript', 'JavaScript'];
+
+let niji: number[][] = [
+  [1, 2, 3],
+  [10, 100, 1000]
+];
+
+let array: (string | number | boolean)[] = [1, false, 'foo'];
---
+++ src/number-string.ts
@@ -0,0 +1,15 @@
+export {};
+
+let year: number = 1996;
+
+console.log(year);
+
+let age: number = 0x18;
+// age = false; 
+
+console.log(age);
+
+let name: string = 'tama';
+// name = 10;
+
+console.log(name);
